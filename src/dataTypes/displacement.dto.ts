bd32f303c8cc6afeacf1c7166a4ad37ffb570614
paulo-fs <jr.paulo87@gmail.com>
2023-06-18 14:28:10 -0300

    feat: add type definitions for displacements

---
+++ src/dataTypes/displacement.dto.ts
@@ -0,0 +1,35 @@
+export interface IDisplacementCreate {
+  kmInicial: number;
+  inicioDeslocamento: Date;
+  checkList: string;
+  motivo: string;
+  observacao: string;
+  idCondutor: number;
+  idVeiculo: number;
+  idCliente: number;
+}
+
+export interface IDisplacementUpdate {
+  id: string;
+  kmFinal: number;
+  fimDeslocamento: Date;
+  obervacao: string;
+}
+
+export interface IDisplacementDelete {
+  id: string;
+}
+
+export interface IDisplacement {
+  id: string;
+  kmInicial: number;
+  kmFinal: number;
+  inicioDeslocamento: Date;
+  imDeslocamento: Date;
+  checkList: string;
+  motivo: string;
+  observacao: string;
+  idCondutor: number;
+  idVeiculo: number;
+  idCliente: number;
+}
