ff4713df0d1d093d2163297fb1a36d9d03673373
paulo-fs <jr.paulo87@gmail.com>
2023-06-18 14:27:23 -0300

    feat: add types definition for users

---
+++ src/dataTypes/passanger.dto.ts
@@ -0,0 +1,36 @@
+export interface IUserCreate {
+  numeroDocumento: string;
+  tipoDocumento: string;
+  nome: string;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+}
+
+export interface IUserUpdate {
+  id: string;
+  nome: string;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+}
+
+export interface IUserDelete {
+  id: string;
+}
+
+export interface IUser {
+  id: string;
+  numeroDocumento: string;
+  tipoDocumento: string;
+  nome: string;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+}
