4728639035089e1ef90ff389e0111be980949daa
paulo-fs <jr.paulo87@gmail.com>
2023-06-19 17:46:06 -0300

    feat: implement displacement requests and logic

--- src/pages/passageiro/[id]/passanger.controller.ts
+++ src/pages/passageiro/[id]/passanger.controller.ts
@@ -1,4 +1,16 @@
-import { startDisplacement } from "@/services/requests/displacement.request";
+import { useMySnackBar } from "@/components/MySnackBar/MySnackBar.controller";
+import { useNotificationModal } from "@/components/NotificationModal/NotificationModal.controller";
+import { IDisplacementOnStore } from "@/dataTypes/displacement.dto";
+import {
+  removeDisplacementOnStore,
+  saveDisplacementOnStore,
+} from "@/helpers/displacementStore";
+import {
+  finishDisplacement,
+  getDisplacementById,
+  startDisplacement,
+} from "@/services/requests/displacement.request";
+import { useRouter } from "next/router";
 import React from "react";
 
 export function usePassanger(userId: string | undefined) {
@@ -6,11 +18,46 @@ export function usePassanger(userId: string | undefined) {
   const [driverId, setDriverId] = React.useState("");
   const [carModel, setCarModel] = React.useState("");
   const [carId, setCarId] = React.useState("");
+  const [activeDisplacement, setActiveDisplacement] =
+    React.useState<IDisplacementOnStore | null>(null);
 
-  const [displacementId, setDisplacementId] = React.useState("");
-  const [errorMessage, setErrorMessage] = React.useState("");
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const {
+    closeNotificationModal,
+    defineNotificationModalInfos,
+    isModalOpen,
+    modalInfos,
+  } = useNotificationModal();
+  const {
+    isSnackbarOpen,
+    snackbarInfos,
+    handleMySnackBar,
+    defineMySnackbarInfos,
+  } = useMySnackBar();
+
+  const router = useRouter();
   const isDisabled = driverName.length === 0 || carModel.length === 0;
+
+  if (userId === undefined) {
+    router.replace("/");
+  }
+
+  async function getActiveDisplacement(id: string) {
+    const { displacement } = await getDisplacementById(id);
+
+    defineMySnackbarInfos({
+      type: "success",
+      message: "Deslocamento iniciado com sucesso.",
+    });
+
+    const currentDisplacement = {
+      id: displacement.id,
+      userId: displacement.idCliente,
+      startDisplacement: displacement.inicioDeslocamento,
+      endDisplacement: displacement.fimDeslocamento,
+    };
+    saveDisplacementOnStore(currentDisplacement);
+    setActiveDisplacement(currentDisplacement);
+  }
 
   async function handleStartDisplacement() {
     if (userId === undefined || driverId.length === 0 || carId.length === 0)
@@ -22,18 +69,36 @@ export function usePassanger(userId: string | undefined) {
         idCondutor: Number(driverId),
         idVeiculo: Number(carId),
       });
-      setDisplacementId(displacementId);
+      getActiveDisplacement(displacementId);
     } catch (err: any) {
       const message =
         err.response.data ??
         "Um erro desconhecido ocorreu, tente novamente mais tarde.";
-      setErrorMessage(message);
-      setIsModalOpen(true);
+      defineNotificationModalInfos({
+        message: message,
+        title: "Ops, algo deu errado",
+        error: true,
+      });
     }
   }
 
-  function handleCloseModal() {
-    setIsModalOpen(!isModalOpen);
+  async function handleFinishDisplacement() {
+    if (!activeDisplacement) return;
+
+    try {
+      await finishDisplacement({
+        id: activeDisplacement.id,
+        startTime: activeDisplacement.startDisplacement,
+      });
+      removeDisplacementOnStore(userId!);
+      setActiveDisplacement(null);
+      defineMySnackbarInfos({
+        type: "success",
+        message: "Deslocamento finalizado com sucesso.",
+      });
+    } catch (err: any) {
+      console.log(err.response.data);
+    }
   }
 
   return {
@@ -45,8 +110,13 @@ export function usePassanger(userId: string | undefined) {
     setCarId,
     isDisabled,
     handleStartDisplacement,
-    errorMessage,
+    activeDisplacement,
+    handleFinishDisplacement,
+    closeNotificationModal,
     isModalOpen,
-    handleCloseModal,
+    modalInfos,
+    isSnackbarOpen,
+    snackbarInfos,
+    handleMySnackBar,
   };
 }
