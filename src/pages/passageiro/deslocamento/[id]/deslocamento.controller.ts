123ba6df3c8c59191b76b6518d303b498a5f87a8
paulo-fs <jr.paulo87@gmail.com>
2023-06-23 21:01:13 -0300

    feat: add displacement  page for user

---
+++ src/pages/passageiro/deslocamento/[id]/deslocamento.controller.ts
@@ -0,0 +1,76 @@
+import { IMenuLink } from "@/components/HeaderMenu/HeaderMenu.props";
+import { IDisplacement } from "@/dataTypes/displacement.dto";
+import { IUser } from "@/dataTypes/passanger.dto";
+import dayjs from "dayjs";
+import React from "react";
+
+export function useDeslocamentoPage(
+  user: IUser | null,
+  displacements: IDisplacement[] | null
+) {
+  const [searchInputValue, setSearchInputValue] = React.useState("");
+  const menuLinks: IMenuLink[] = [
+    {
+      title: "Perfil",
+      url: `passageiro/${user?.id}`,
+    },
+    {
+      title: "Deslocamentos",
+      url: `passageiro/deslocamento/${user?.id}`,
+    },
+  ];
+
+  const tableHead = [
+    { label: "id" },
+    { label: "Data" },
+    { label: "Total Percorrido (km)" },
+    { label: "Km Inicial" },
+    { label: "Km Final" },
+  ];
+
+  const tableData = displacements?.map((item) => {
+    const total = item.kmFinal - item.kmInicial;
+    const totalPositive = total < 0 ? total * -1 : total;
+    const kmResult = totalPositive === 0 ? "Alguns metros" : totalPositive;
+    return {
+      id: item.id,
+      data: dayjs(item.inicioDeslocamento).format("DD/MM/YYYY"),
+      kmTotal: kmResult,
+      kmInicial: item.kmInicial ?? 0,
+      kmFinal: item.kmFinal ?? 0,
+    };
+  });
+
+  const filteredTableData = search();
+
+  function search() {
+    if (!searchInputValue) return;
+    const result = tableData?.filter((item) => {
+      return (
+        item.data.includes(searchInputValue) ||
+        String(item.kmTotal).includes(searchInputValue) ||
+        String(item.kmInicial).includes(searchInputValue) ||
+        String(item.kmFinal).includes(searchInputValue)
+      );
+    });
+    return result;
+  }
+
+  function clearSearchInput() {
+    setSearchInputValue("");
+  }
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchInputValue(event.target.value);
+  }
+
+  return {
+    menuLinks,
+    tableHead,
+    tableData,
+    filteredTableData,
+    searchInputValue,
+    handleSearch,
+    clearSearchInput,
+  };
+}
---
+++ src/pages/passageiro/deslocamento/[id]/index.page.tsx
@@ -0,0 +1,93 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+import { Container, Grid, Typography } from "@mui/material";
+import { HeaderMenu, MyTable, SearchInput } from "@/components";
+
+import { IDisplacement } from "@/dataTypes/displacement.dto";
+import { IUser } from "@/dataTypes/passanger.dto";
+import { getUserById } from "@/services/requests/user.request";
+
+import { getAllDisplacements } from "@/services/requests/displacement.request";
+import { useDeslocamentoPage } from "./deslocamento.controller";
+
+export default function DriverDisplacement({ user, displacements } : InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const {
+    menuLinks,
+    tableHead,
+    tableData,
+    filteredTableData,
+    searchInputValue,
+    handleSearch,
+    clearSearchInput,
+  } = useDeslocamentoPage(user, displacements)
+
+  return (
+    <>
+      <HeaderMenu
+        pages={menuLinks}
+      />
+      <Container maxWidth='xl'>
+        {/* head description */}
+        <Grid container marginTop={14} >
+          <Grid item sm={6} padding={2}>
+            <Typography component='h1' variant="h4" sx={{ textTransform: 'capitalize' }}>
+              Deslocamentos
+            </Typography>
+            <Typography variant="body1">
+              Aqui estão listadas todos os deslocamentos que você realizou.
+            </Typography>
+          </Grid>
+        </Grid>
+
+        {/* content */}
+        <Grid container marginTop={4} paddingX={4}>
+          <Grid item xs={12}>
+            <SearchInput
+              clearSearchInput={clearSearchInput}
+              handleSearch={handleSearch}
+              searchInputValue={searchInputValue}
+            />
+          </Grid>
+
+          <MyTable
+            tableHead={tableHead}
+            data={filteredTableData ?? tableData ?? []}
+          />
+        </Grid>
+      </Container>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  user: IUser | null;
+  displacements: IDisplacement[] | null
+}> = async ({ req, res, params }) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=49"
+  );
+
+  const userId = String(params?.id);
+
+  try {
+    const { user } = await getUserById(userId);
+    const { displacements } = await getAllDisplacements()
+
+    const driverDisplacements = displacements.filter(item => String(item.idCliente) === userId)
+
+    return {
+      props: {
+        user,
+        displacements: driverDisplacements
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        user: null,
+        displacements: null
+      },
+    };
+  }
+};
